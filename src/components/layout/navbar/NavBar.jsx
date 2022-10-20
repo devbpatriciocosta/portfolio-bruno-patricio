7bc3c598b42d31199220cce4cd7eeecd4737b416
Bruno Patrício <bpatriciocosta@gmail.com>
2022-10-20 14:09:53 -0300

    Nav Bar Responsiva

---
+++ src/components/layout/navbar/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+import Burguer from "./Burguer";
+import LeftNav from "./LeftNavBar";
+
+const StyledNavBar = styled.nav`
+  display: flex;
+  gap: 30px;
+  width: 100%;
+  height: 70px;
+  padding: 10px 0;
+  position: fixed;
+  background-color: ${props => props.theme.background};
+
+  @media (max-width: 868px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
+export default function NavBar() {
+  return (
+    <StyledNavBar>
+      <LeftNav />
+      <Burguer />
+    </StyledNavBar>
+  )
+}
