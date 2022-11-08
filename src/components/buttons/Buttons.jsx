ea72ebd2d3cc5f2a419997394b24aef32471a885
Bruno Patrício <bpatriciocosta@gmail.com>
2022-11-08 15:04:48 -0300

    Ajuste de botões

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -1,23 +1,19 @@
 import styled from 'styled-components'
 
 const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-width: 20rem;
-  align-items: center;
-
-  label {
-    display: flex;
-    font-weight: bold;
-  }
   
   button {
     background-color: ${(props) => props.theme.primary};
     color: ${(props) => props.theme.background};
     cursor: pointer;
     font-weight: bold;
-    padding: 15px;
-    border-radius: 10px;
+    padding: 15px 35px;
+    border-radius: 15px;
+    transition: all ease-in-out 0.3s;
+  }
+
+  :hover {
+
   }
 `
 
