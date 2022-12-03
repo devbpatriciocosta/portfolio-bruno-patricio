316dff4c05b016fd1225b789e19ef56b487eb296
Bruno Patrício <bpatriciocosta@gmail.com>
2022-12-03 20:24:30 -0300

    Ajuste no botão

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -1,26 +1,28 @@
 import styled from 'styled-components'
 
-const ButtonContainer = styled.div`
-  
-  button {
-    background-color: ${(props) => props.theme.primary};
-    color: ${(props) => props.theme.background};
-    cursor: pointer;
-    font-weight: bold;
-    padding: 15px 35px;
-    border-radius: 15px;
-    transition: all ease-in-out 0.3s;
-  }
+const StyledButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.background};
+  width: 120px;
+  height: 50px;
+  font-weight: bold;
+  border-radius: 10px;
+  transition: all ease-in-out 0.3s;
+
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
 
   :hover {
-
+    transform: scale(1.1);
+    background-color: ${(props) => props.theme.secondary};
   }
 `
 
 export default function Button(props) {
   return (
-    <ButtonContainer>
-      <button {...props}>{props.children}</button>
-    </ButtonContainer>
+      <StyledButton {...props}><p>{props.children}</p></StyledButton>
   )
 }
