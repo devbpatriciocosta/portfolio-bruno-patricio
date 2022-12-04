704cb30595bc770da3e5b3b0ab3f2da7ed630626
Bruno Patrício <bpatriciocosta@gmail.com>
2022-12-04 18:00:36 -0300

    Add about in indedx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import NavBar from "../src/components/layout/navbar/NavBar";
 import Intro from "../src/components/layout/intro/Intro";
+import AboutMe from '../src/components/layout/about/About'
 
 function HomePage() {
   return (
     <>
       <NavBar />
       <Intro />
+      <AboutMe />
     </>
   )
 }
