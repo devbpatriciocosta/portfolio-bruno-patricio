18f603934bf80f7e6e04564e17d80b498c19f08f
Bruno Patrício <bpatriciocosta@gmail.com>
2022-12-05 09:25:08 -0300

    Adding secondary background

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,7 @@ const theme = {
   primary: '#FEC350',
   secondary: '#E8BB00',
   background: '#000000',
+  secondaryBackground: '#FEC350',
   lineBar: '#1E1E1E',
   postBackgroundColor: '#d1d1d1'
 }
