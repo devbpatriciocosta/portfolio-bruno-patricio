5857918a45c663a0b2e1463853ea6b09476b8dcf
Bruno Patrício <bpatriciocosta@gmail.com>
2022-12-06 09:43:19 -0300

    Adding info at index

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import NavBar from "../src/components/layout/navbar/NavBar";
 import Intro from "../src/components/layout/intro/Intro";
 import AboutMe from '../src/components/layout/about/About'
 import WorksDeveloped from "../src/components/works/Works";
+import Education from "../src/components/layout/Education/Education";
 
 function HomePage() {
   return (
@@ -10,6 +11,7 @@ function HomePage() {
       <Intro />
       <AboutMe />
       <WorksDeveloped />
+      <Education />
     </>
   )
 }
