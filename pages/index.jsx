587291cd48be5ffd1025054628f6b96a4c0b50ed
Bruno Patrício <bpatriciocosta@gmail.com>
2022-12-07 12:07:05 -0300

    Adjust in imports

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import NavBar from "../src/components/layout/navbar/NavBar";
 import Intro from "../src/components/layout/intro/Intro";
 import AboutMe from '../src/components/layout/about/About'
 import WorksDeveloped from "../src/components/works/Works";
-import Education from "../src/components/layout/Education/Education";
+import Education from "../src/components/layout/education/Education";
 import ContactSession from "../src/components/layout/contact/Contact";
 import Footer from "../src/components/footer/Footer";
 
